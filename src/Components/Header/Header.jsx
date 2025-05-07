1b9e2846bd9ed6ec6aa0596d5053cf9f83a1c9f3
Afmarnob <afmarnob@gmail.com>
2025-05-08 05:05:26 +0600

    done

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,15 +3,12 @@ import nav1 from '../../assets/svg file/nav1.svg';
 
 const Header = () => {
     return (
-        <header className="mt-6 px-4">
+        <header className="mt-4 px-4 mb-9">
             <nav className="navbar max-w-[1140px] mx-auto px-6 py-2 rounded-full bg-[#111820] border-2 border-gray-500 backdrop-blur-md text-white">
-                {/* Left: Logo */}
                 <div className="flex items-center space-x-3">
                     <button className="btn btn-ghost btn-circle">
-                        <img className="w-8 h-8 object-contain" src={nav1} alt="Logo" />
+                        <img className="w-38 h-18 object-contain" src={nav1} alt="Logo" />
                     </button>
-
-                    {/* Mobile Menu Button (visible on small screens) */}
                     <div className="dropdown lg:hidden">
                         <button tabIndex={0} className="btn btn-ghost btn-circle">
                             <HiBars2 size={24} />
@@ -24,16 +21,12 @@ const Header = () => {
                         </ul>
                     </div>
                 </div>
-
-                {/* Center: Desktop Menu */}
                 <ul className="hidden lg:flex menu menu-horizontal space-x-6 text-sm font-medium ml-auto">
                     <li><a className="hover:text-gray-300">About</a></li>
                     <li><a className="hover:text-gray-300">Blog</a></li>
                     <li><a className="hover:text-gray-300">Work</a></li>
                     <li><a className="hover:text-gray-300">Contact</a></li>
                 </ul>
-
-                {/* Right: Extra Icon (Optional) */}
                 <div className="hidden lg:flex ml-6">
                     <button className="btn btn-ghost btn-circle">
                         <HiBars2 size={24} />
